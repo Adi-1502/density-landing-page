6ad8dd322636cc8f4cc5fef4926cb7e14143dda6
garvit-density <garvit.intern@density.exchange>
2023-04-02 19:15:57 +0530

    added cursor animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,48 @@ import GetEarlyAccess from "@/components/GetEarlyAccess";
 import Cards from "@/components/Cards";
 import FaqSection from "@/components/Faq";
 import SocialMedia from "@/components/SocialMedia";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "yellow",
+      mixBlendMode: "difference",
+    },
+  };
+
   return (
     <>
       <NavBar />
@@ -18,6 +56,11 @@ export default function Home() {
       <Cards />
       <FaqSection />
       <SocialMedia></SocialMedia>
+      <motion.div
+        className="cursor"
+        variants={variants}
+        animate={cursorVariant}
+      />
     </>
   );
 }
